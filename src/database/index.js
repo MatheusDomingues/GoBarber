d483417aba83545f81aba17da4ff0794fd71752b
Matheus Domingues <matheus_luige@hotmail.com>
2020-05-25 18:23:02 -0300

    back-end do app

---
+++ src/database/index.js
@@ -0,0 +1,22 @@
+import Sequelize from 'sequelize';
+
+import User from '../app/models/User';
+
+import databaseConfig from '../config/database';
+
+const models = [User];
+
+class Database {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    this.connection = new Sequelize(databaseConfig);
+
+    // eslint-disable-next-line prettier/prettier
+    models.map(model => model.init(this.connection));
+  }
+}
+
+export default new Database();
